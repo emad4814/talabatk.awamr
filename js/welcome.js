cda272f35c9c172eb275e09d1ec6938cf660b159
Emad mohammad <emad.saad4814@gmail.com>
2026-01-03 10:10:01 +0200

    Add files via upload

---
+++ js/welcome.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var closeBtn = document.getElementById("closeBtn");
+    var overlay = document.getElementById("welcomeOverlay");
+
+    // الزر لإغلاق الـ popup
+    closeBtn.addEventListener("click", function () {
+        overlay.style.display = "none"
+    })
+})
+
+
+
+// ============ login form =============
+
+// Retro Future Login Form JavaScript
+
+
